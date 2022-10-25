3dcad11b6782027be07ca69ee70f564096f2cffc
Adolfo Herrera <valherdolf@gmail.com>
2022-10-24 20:53:11 -0400

    Import Header component

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import Header from '../components/Header';
 
 export default function Root() {
   return (
     <>
-      <header>Title</header>
-      <div>
+      <Header />
+      <main>
         <Outlet />
-      </div>
+      </main>
     </>
   );
 }
