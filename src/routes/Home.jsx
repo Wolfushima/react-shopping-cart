3a1099f877a1bb4c8630671a5c7fc770c9cdc054
Adolfo Herrera <valherdolf@gmail.com>
2022-10-25 18:46:24 -0400

    Home route structure

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
+import howlsMovingCastleMP4 from '../assets/videos/howls-moving-castle.mp4';
+import spiritedAwayMP4 from '../assets/videos/spirited-away.mp4';
 
 const Home = () => (
-  <div>
-    Home Page
-  </div>
+  <>
+    <article className="home-article home-article-one">
+      <div className="overlay" />
+    </article>
+    <article className="home-article home-article-two">
+      <video autoPlay loop muted playsInline>
+        <source src={howlsMovingCastleMP4} type="video/mp4" />
+      </video>
+    </article>
+    <article className="home-article home-article-three" />
+    <article className="home-article home-article-four">
+      <video autoPlay loop muted playsInline>
+        <source src={spiritedAwayMP4} type="video/mp4" />
+      </video>
+    </article>
+    <article className="home-article home-article-five" />
+  </>
 );
 
 export default Home;
