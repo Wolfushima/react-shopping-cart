4a85c1792654dc7a184c22bc847002d063c486a8
Adolfo Herrera <valherdolf@gmail.com>
2022-10-25 18:32:29 -0400

    Update Header structure and style

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import logoWordMarkSvg from '../assets/images/Studio_Ghibli_wordmark.svg';
 import logoGif from '../assets/gifs/logo.gif';
 import addToCardIcon from '../assets/images/add-to-cart.png';
@@ -8,7 +9,7 @@ const Header = () => (
   <header>
     <div className="header-container">
       <div className="header-logo">
-        <a href="/" className="home-link">
+        <Link to="/" className="home-link">
           <div className="logo-container">
             <div className="logo-gif-container">
               <img className="logo logo-gif" src={logoGif} alt="logo" />
@@ -17,37 +18,37 @@ const Header = () => (
               <img className="logo logo-wordmark" src={logoWordMarkSvg} alt="Studio Ghibli" />
             </div>
           </div>
-        </a>
+        </Link>
       </div>
 
       <div className="header-links">
         <div className="link-container">
-          <a href="/shop" className="link">Shop the Collection</a>
+          <Link to="shop" className="link">Shop the Collection</Link>
         </div>
         <div className="link-container">
-          <a href="/browse" className="link">All Films</a>
+          <Link to="browse" className="link">All Films</Link>
         </div>
 
       </div>
 
       <div className="header-controls">
-        <a href="/" className="header-controls__search">
+        <Link to="/" className="header-controls__search">
           <div className="icon-container">
             <img className="icon icon-search" src={searchIcon} alt="Search Icon" />
           </div>
-        </a>
-        <a href="/cart" className="header-controls__cart">
+        </Link>
+        <Link to="cart" className="header-controls__cart">
           <div className="icon-container">
             <img className="icon icon-cart" src={addToCardIcon} alt="Cart Icon" />
           </div>
-        </a>
-        <a href="/" className="header-controls__nav">
+        </Link>
+        <Link to="/" className="header-controls__nav">
           <div className="hamburger-icon" id="icon">
             <div className="icon-1" id="a" />
             <div className="icon-2" id="b" />
             <div className="icon-3" id="c" />
           </div>
-        </a>
+        </Link>
       </div>
     </div>
   </header>
