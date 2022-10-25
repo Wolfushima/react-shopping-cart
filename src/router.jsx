fd69a672755ff6acb97e8c9847c91c13f4c31389
Adolfo Herrera <valherdolf@gmail.com>
2022-10-25 18:48:53 -0400

    Create error-page component, import error-page component to router

--- src/router.jsx
+++ src/router.jsx
@@ -1,16 +1,23 @@
 import React from 'react';
 import { createBrowserRouter } from 'react-router-dom';
+import ErrorPage from './error-page';
 import Root from './routes/root';
 import Home from './routes/Home';
+import Shop from './routes/shop/Shop';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Root />,
+    errorElement: <ErrorPage />,
     children: [
       {
         index: true,
         element: <Home />,
+      },
+      {
+        path: '/shop',
+        element: <Shop />,
       },
     ],
   },
