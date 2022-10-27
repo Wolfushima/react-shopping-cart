76434bc8f504143c1b230babe8b4066b645a8693
Adolfo Herrera <valherdolf@gmail.com>
2022-10-27 19:45:55 -0400

    Create ShopHeader Component

---
+++ src/components/shop/ShopHeader.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const ShopHeader = () => (
+  <header className="root-subheader">
+    <div className="root-subheader__container">
+      <h1 className="root-subheader__title">
+        SHOP THE
+        <br />
+        COLLECTION
+      </h1>
+    </div>
+  </header>
+);
+
+export default ShopHeader;
