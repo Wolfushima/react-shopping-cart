89862c6b3f48805e4c39294dd0429e70280f199a
Adolfo Herrera <valherdolf@gmail.com>
2022-10-31 15:58:37 -0400

    Implemenet scroll down btn to home articles

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -4,21 +4,67 @@ import spiritedAwayMP4 from '../assets/videos/spirited-away.mp4';
 
 const Home = () => (
   <>
-    <article className="home-article home-article-one">
-      <div className="overlay" />
+    <article id="home-article-one" className="home-article home-article-one">
+      <div className="home-article-one__container">
+        <div className="home-scroll-down-btn">
+          <button
+            className="scroll-down-btn"
+            aria-label="Scroll Down"
+            type="button"
+            onClick={() => {
+              document.getElementById('home-article-two').scrollIntoView({ behavior: 'smooth', block: 'end', inline: 'nearest' });
+            }}
+          />
+        </div>
+      </div>
     </article>
-    <article className="home-article home-article-two">
+
+    <article id="home-article-two" className="home-article home-article-two">
       <video autoPlay loop muted playsInline>
         <source src={howlsMovingCastleMP4} type="video/mp4" />
       </video>
+      <div className="home-scroll-down-btn">
+        <button
+          className="scroll-down-btn"
+          aria-label="Scroll Down"
+          type="button"
+          onClick={() => {
+            document.getElementById('home-article-three').scrollIntoView({ behavior: 'smooth', block: 'end', inline: 'nearest' });
+          }}
+        />
+      </div>
     </article>
-    <article className="home-article home-article-three" />
-    <article className="home-article home-article-four">
+
+    <article id="home-article-three" className="home-article home-article-three">
+      <div className="home-scroll-down-btn">
+        <button
+          className="scroll-down-btn"
+          aria-label="Scroll Down"
+          type="button"
+          onClick={() => {
+            document.getElementById('home-article-four').scrollIntoView({ behavior: 'smooth', block: 'end', inline: 'nearest' });
+          }}
+        />
+      </div>
+    </article>
+
+    <article id="home-article-four" className="home-article home-article-four">
       <video autoPlay loop muted playsInline>
         <source src={spiritedAwayMP4} type="video/mp4" />
       </video>
+      <div className="home-scroll-down-btn">
+        <button
+          className="scroll-down-btn"
+          aria-label="Scroll Down"
+          type="button"
+          onClick={() => {
+            document.getElementById('home-article-five').scrollIntoView({ behavior: 'smooth', block: 'end', inline: 'nearest' });
+          }}
+        />
+      </div>
     </article>
-    <article className="home-article home-article-five" />
+
+    <article id="home-article-five" className="home-article home-article-five" />
   </>
 );
 
